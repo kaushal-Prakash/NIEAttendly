729db71659049bbcf5a290ba19fb99d380d17acc
Kaushal Prakash <kingkaushal1289@gmail.com>
2024-11-16 19:39:52 +0530

    revised signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,13 +1,11 @@
 "use client"; 
 import Link from "next/link"; 
 import React, { useEffect, useState } from "react"; 
-import { useRouter } from "next/navigation"; 
 import axios from "axios"; 
 import { toast, Toaster } from "react-hot-toast"; 
 import Image from "next/image";
 
 export default function SignupPage() {
-  const router = useRouter();
   const [user, setUser] = useState({
     email: "",
     password: "",
