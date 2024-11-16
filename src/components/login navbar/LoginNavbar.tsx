d5e36d88d545c84ce510845c74641c5ac25c0eb1
Kaushal Prakash <kingkaushal1289@gmail.com>
2024-11-16 20:38:22 +0530

    added view report

--- src/components/login navbar/LoginNavbar.tsx
+++ src/components/login navbar/LoginNavbar.tsx
@@ -1,7 +1,6 @@
 'use client'
 import LogoutButton from '../logout btn/LogoutBtn'
 import Link from 'next/link'
-import { LuUserCircle2 } from "react-icons/lu";
 import gsap from 'gsap';
 import { useEffect } from 'react';
 
@@ -20,9 +19,9 @@ function LoginNavbar() {
 
   return (
     <div className='w-full login-nav-container max-w-xs z-10 mx-auto bg-transparent backdrop-blur-md mt-20 flex gap-2 border-2 shadow-sm rounded-md mb-5 justify-center items-center p-3'>
-        <div className='login-nav-optn'>
+        <div className='login-nav-optn bg-green-600 text-white font-semibold px-2 py-2 rounded-lg'>
             <Link href="/stats" title='Stats'>
-                <LuUserCircle2 size={24} className='hover:-translate-y-1 -translate-x-1 duration-200' />
+                View Report
             </Link>
         </div>
         <div className='login-nav-optn'>
